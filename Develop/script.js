b853a15d12826bd02e0d214ed6d0aa2e0c70894a
Sheridan Melton <99241310+XxElijahMxX@users.noreply.github.com>
2022-03-18 20:59:08 -0400

    Update script.js
    updated conditional statement

--- Develop/script.js
+++ Develop/script.js
@@ -12,23 +12,28 @@ function generatePassword() {
 // Prompt user if using lower case, uppercase, numbers, special characters
   var passLength = prompt("How many characters should this password be? Pick a number between 8 and 128.");
 
-  if ((passLength >= 8) && (passLength <= 128))
+  if ((passLength >= 8) && (passLength <= 128)) {
+   
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
+    var password = ""
 
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
-  var password = ""
-
-  var genSymbols = confirm("Would you like to use special characters? i.e. (!, $, %, etc.)");
-  var genNumbers = confirm("Would you like to use numbers?");
-  var genLowerCase = confirm("Would you like to use lowercase letters?");
-  var genUpperCase = confirm("Would you like to use uppercase letter?");
-
-// 3. Generate password based on criteria
-  for (var i=0; i<passLength; i++){
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber+1);
+    var genSymbols = confirm("Would you like to use special characters? i.e. (!, $, %, etc.)");
+    var genNumbers = confirm("Would you like to use numbers?");
+    var genLowerCase = confirm("Would you like to use lowercase letters?");
+    var genUpperCase = confirm("Would you like to use uppercase letter?");
+    
+      for (var i=0; i<passLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+      }
+       return password += chars.substring(randomNumber, randomNumber)+1;
   }
+
+ 
+// 3. Generate password based on criteria
+  
   // 4. Display generated password on the page
-  return password += chars.substring(randomNumber, randomNumber)+1;
+
 }
 
   
